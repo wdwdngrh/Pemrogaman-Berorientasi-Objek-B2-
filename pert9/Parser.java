ff4754050b9c9342bd421469feaedc800956afa0
wdwdngrh <wdnugraha24@gmail.com>
2025-12-26 09:43:23 +0700

    Add Parser class for command input handling

---
+++ pert9/Parser.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+/**
+ * Write a description of class Parser here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Parser{
+    private CommandWords commands;
+    private Scanner reader;
+    
+    public Parser(){
+        commands = new CommandWords();
+        reader = new Scanner(System.in);
+    }
+    
+    public Command getCommand(){
+        String inputLine;
+        String word1 = null;
+        String word2 = null;
+        
+        System.out.print("> ");
+        
+        inputLine = reader.nextLine();
+        
+        Scanner tokenizer = new Scanner(inputLine);
+        if(tokenizer.hasNext()){
+            word1 = tokenizer.next();
+            
+            if(tokenizer.hasNext()){
+                word2 = tokenizer.next();
+            }
+        }
+        
+        if(commands.isCommand(word1)){
+            return new Command(word1, word2);
+        }
+        else{
+            return new Command(null, word2);
+        }
+    }
+}
