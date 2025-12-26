520c8b25332cc7cf748905ce731eb8ebfd954c2b
wdwdngrh <wdnugraha24@gmail.com>
2025-12-26 13:44:36 +0700

    Create MainSimulator class to execute simulation
    Initial implementation of MainSimulator class to run a simulation.

---
+++ pert12/Fox and Rabbit/MainSimulator.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class MainSimulator here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MainSimulator
+{
+    public static void main(String[] args)
+    {
+        Simulator sim = new Simulator(150, 150);
+        sim.runLongSimulation();
+    }
+}
