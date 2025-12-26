302df139f8a2cd236da869fbecc65dd58f31317e
wdwdngrh <wdnugraha24@gmail.com>
2025-12-26 09:44:55 +0700

    Add Command class with command handling methods

---
+++ pert9/Command.java
@@ -0,0 +1,31 @@
+/**
+ * Write a description of class Command here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Command{
+    private String commandWord;
+    private String secondWord;
+    
+    public Command(String firstWord, String secondWord){
+        commandWord = firstWord;
+        this.secondWord = secondWord;
+    }
+    
+    public String getCommandWord(){
+        return commandWord;
+    }
+    
+    public String getSecondWord(){
+        return secondWord;
+    }
+    
+    public boolean isUnknown(){
+        return (commandWord == null);
+    }
+    
+    public boolean hasSecondWord(){
+        return(secondWord != null);
+    }
+}
